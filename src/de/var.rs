e736ffebad13f4473e26fc45f7243c49ea3365b5
Jooyung Han <jooyung@google.com>
2021-07-16 01:22:24 +0000

    Revert^2 "Import serde-xml-rs 0.4.1"
    1620dfabbf20dc28ead86e7cfcf97762b6962736
    
    Change-Id: Id7fe06c5cf538afaa6a0b654eab03eb0a0c593ed

---
+++ src/de/var.rs
@@ -0,0 +1,84 @@
+use std::io::Read;
+
+use serde::de::{self, Deserializer as SerdeDeserializer, IntoDeserializer};
+use xml::name::OwnedName;
+use xml::reader::XmlEvent;
+
+use de::Deserializer;
+use error::{Error, Result};
+
+pub struct EnumAccess<'a, R: 'a + Read> {
+    de: &'a mut Deserializer<R>,
+}
+
+impl<'a, R: 'a + Read> EnumAccess<'a, R> {
+    pub fn new(de: &'a mut Deserializer<R>) -> Self {
+        EnumAccess { de: de }
+    }
+}
+
+impl<'de, 'a, R: 'a + Read> de::EnumAccess<'de> for EnumAccess<'a, R> {
+    type Error = Error;
+    type Variant = VariantAccess<'a, R>;
+
+    fn variant_seed<V: de::DeserializeSeed<'de>>(
+        self,
+        seed: V,
+    ) -> Result<(V::Value, VariantAccess<'a, R>)> {
+        let name = expect!(
+            self.de.peek()?,
+
+            &XmlEvent::Characters(ref name) |
+            &XmlEvent::StartElement { name: OwnedName { local_name: ref name, .. }, .. } => {
+                seed.deserialize(name.as_str().into_deserializer())
+            }
+        )?;
+        self.de.set_map_value();
+        Ok((name, VariantAccess::new(self.de)))
+    }
+}
+
+pub struct VariantAccess<'a, R: 'a + Read> {
+    de: &'a mut Deserializer<R>,
+}
+
+impl<'a, R: 'a + Read> VariantAccess<'a, R> {
+    pub fn new(de: &'a mut Deserializer<R>) -> Self {
+        VariantAccess { de: de }
+    }
+}
+
+impl<'de, 'a, R: 'a + Read> de::VariantAccess<'de> for VariantAccess<'a, R> {
+    type Error = Error;
+
+    fn unit_variant(self) -> Result<()> {
+        self.de.unset_map_value();
+        match self.de.next()? {
+            XmlEvent::StartElement {
+                name, attributes, ..
+            } => if attributes.is_empty() {
+                self.de.expect_end_element(name)
+            } else {
+                Err(de::Error::invalid_length(attributes.len(), &"0"))
+            },
+            XmlEvent::Characters(_) => Ok(()),
+            _ => unreachable!(),
+        }
+    }
+
+    fn newtype_variant_seed<T: de::DeserializeSeed<'de>>(self, seed: T) -> Result<T::Value> {
+        seed.deserialize(&mut *self.de)
+    }
+
+    fn tuple_variant<V: de::Visitor<'de>>(self, len: usize, visitor: V) -> Result<V::Value> {
+        self.de.deserialize_tuple(len, visitor)
+    }
+
+    fn struct_variant<V: de::Visitor<'de>>(
+        self,
+        _fields: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value> {
+        self.de.deserialize_map(visitor)
+    }
+}
---
+++ tests/test.rs
@@ -0,0 +1,161 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_xml_rs;
+
+extern crate log;
+extern crate simple_logger;
+
+use serde_xml_rs::from_str;
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct Item {
+    name: String,
+    source: String,
+}
+
+#[test]
+fn simple_struct_from_attributes() {
+    let _ = simple_logger::init();
+
+    let s = r##"
+        <item name="hello" source="world.rs" />
+    "##;
+
+    let item: Item = from_str(s).unwrap();
+
+    assert_eq!(
+        item,
+        Item {
+            name: "hello".to_string(),
+            source: "world.rs".to_string(),
+        }
+    );
+}
+
+#[test]
+fn multiple_roots_attributes() {
+    let _ = simple_logger::init();
+
+    let s = r##"
+        <item name="hello" source="world.rs" />
+        <item name="hello" source="world.rs" />
+    "##;
+
+    let item: Vec<Item> = from_str(s).unwrap();
+
+    assert_eq!(
+        item,
+        vec![
+            Item {
+                name: "hello".to_string(),
+                source: "world.rs".to_string(),
+            },
+            Item {
+                name: "hello".to_string(),
+                source: "world.rs".to_string(),
+            },
+        ]
+    );
+}
+
+#[test]
+fn simple_struct_from_attribute_and_child() {
+    let _ = simple_logger::init();
+
+    let s = r##"
+        <item name="hello">
+            <source>world.rs</source>
+        </item>
+    "##;
+
+    let item: Item = from_str(s).unwrap();
+
+    assert_eq!(
+        item,
+        Item {
+            name: "hello".to_string(),
+            source: "world.rs".to_string(),
+        }
+    );
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct Project {
+    name: String,
+
+    #[serde(rename = "item", default)]
+    items: Vec<Item>,
+}
+
+#[test]
+fn nested_collection() {
+    let _ = simple_logger::init();
+
+    let s = r##"
+        <project name="my_project">
+            <item name="hello1" source="world1.rs" />
+            <item name="hello2" source="world2.rs" />
+        </project>
+    "##;
+
+    let project: Project = from_str(s).unwrap();
+
+    assert_eq!(
+        project,
+        Project {
+            name: "my_project".to_string(),
+            items: vec![
+                Item {
+                    name: "hello1".to_string(),
+                    source: "world1.rs".to_string(),
+                },
+                Item {
+                    name: "hello2".to_string(),
+                    source: "world2.rs".to_string(),
+                },
+            ],
+        }
+    );
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+enum MyEnum {
+    A(String),
+    B { name: String, flag: bool },
+    C,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct MyEnums {
+    #[serde(rename = "$value")]
+    items: Vec<MyEnum>,
+}
+
+#[test]
+fn collection_of_enums() {
+    let _ = simple_logger::init();
+
+    let s = r##"
+        <enums>
+            <A>test</A>
+            <B name="hello" flag="true" />
+            <C />
+        </enums>
+    "##;
+
+    let project: MyEnums = from_str(s).unwrap();
+
+    assert_eq!(
+        project,
+        MyEnums {
+            items: vec![
+                MyEnum::A("test".to_string()),
+                MyEnum::B {
+                    name: "hello".to_string(),
+                    flag: true,
+                },
+                MyEnum::C,
+            ],
+        }
+    );
+}
